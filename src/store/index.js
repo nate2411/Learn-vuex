d9b5be634225b116aa78c60771b8aa1aee44defd
nate2411 <nathandj28@gmail.com>
2021-11-10 15:03:46 +0200

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,21 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    counter: 0
   },
   mutations: {
+    increaseCounter(state){
+      state.counter++
+    },
+    decreaseCounter(state){
+      state.counter--
+    }
   },
   actions: {
+   increaseCounter(){
+     console.log('increaseCounter(action)')
+   } 
   },
   modules: {
-  }
+  } 
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="counter">
+     {{$store.state.counter}}
+    </div>
+    <div class="buttons">
+      <button @click="$store.commit('decreaseCounter')">-</button>
+      <button @click="$store.dispatch('increaseCounter') ">+</button>
+    </div>
+
+  
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+ name: 'Home'
 }
 </script>
+
+<style>
+div{
+  margin-bottom: 10px;
+
+}
+.counter{
+  font-size:80px;
+}
+
+.buttons button{
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+
+</style>
